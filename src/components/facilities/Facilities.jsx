6f3ba8d44ed1332102e47fcdb73627eec5e9a9bf
tweener023 <nikolajsatara99@gmail.com>
2023-01-09 16:54:56 +0100

    connected to backend
    loaded real data to facilities instead of dummy data

--- src/components/facilities/Facilities.jsx
+++ src/components/facilities/Facilities.jsx
@@ -3,50 +3,18 @@ import {useState} from "react";
 import axios from 'axios'
 import { Component } from 'react';
 import "./facilities.scss"
-
-
-const facilities = [
-  {
-    name: 'Facility 1',
-    address: '123 Main St',
-    description: 'This is a description of Facility 1',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  },
-  {
-    name: 'Facility 2',
-    address: '123 Main St',
-    description: 'This is a description of Facility 1',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  },
-  {
-    name: 'Facility 3',
-    address: '123 Main St',
-    description: 'This is a description of Facility 1',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  },
-  {
-    name: 'Facility 4',
-    address: '123 Main St',
-    description: 'This is a description of Facility 1',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  },
-  {
-    name: 'Facility 5',
-    address: '456 Main St',
-    description: 'This is a description of Facility 2',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  },
-  {
-    name: 'Facility 6',
-    address: '789 Main St',
-    description: 'This is a description of Facility 3',
-    appointments: ['9:00am', '10:00am', '11:00am']
-  }
-];
+import { useEffect } from 'react';
 
 function Facilities() {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedFacility, setSelectedFacility] = useState(null);
+  const [facilities, setFacilities] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/facilites/all')
+      .then(response => response.json())
+      .then(data => setFacilities(data));
+  }, []);
 
   const handleSearchChange = event => {
     setSearchTerm(event.target.value);
@@ -60,8 +28,8 @@ function Facilities() {
     setSelectedFacility(null);
   };
 
-  const filteredFacilities = facilities.filter(facility =>
-    facility.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredFacilities = facilities?.filter(facility =>
+    facility.centerName.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -77,23 +45,23 @@ function Facilities() {
       </header>
       <div className="facility-list">
         {filteredFacilities.map(facility => (
-          <div className="facility" key={facility.name}>
-            <h2>{facility.name}</h2>
-            <p>{facility.address}</p>
+          <div className="facility" key={facility.centerName}>
+            <h2>{facility.centerName}</h2>
+            <p>{facility.centerAddress}</p>
             <button onClick={() => handleFacilitySelect(facility)}>
               See more
             </button>
           </div>
         ))}
       </div>
-      {selectedFacility && (
+      {selectedFacility?.centerAppointments && (
         <div className="facility-overlay">
           <div className="facility-detail">
-            <h2>{selectedFacility.name}</h2>
-            <p>{selectedFacility.description}</p>
+            <h2>{selectedFacility.centerName}</h2>
+            <p>{selectedFacility.centerDescription}</p>
             <h3>Available appointments:</h3>
             <ul>
-              {selectedFacility.appointments.map(appointment => (
+              {selectedFacility.centerAppointments.map(appointment => (
                 <li key={appointment}>{appointment}</li>
               ))}
             </ul>
